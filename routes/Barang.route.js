416cd2a8129ec451bbad3eb7bae8214c4ba73686
Maulidya Ika <102705992+maulidya105@users.noreply.github.com>
2024-07-24 23:09:18 +0700

    Update Barang.route.js

--- routes/Barang.route.js
+++ routes/Barang.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { verifyAccessToken } = require("../helpers/jwt_helper");
 const BarangController = require("../controllers/Barang.controller");
 
-router.get("/", verifyAccessToken, BarangController.getAllBarang);
+router.get("/", verifyAccessToken, BarangController.getAllBarang); //function verifyAccessToken digunakan sebagai middleware untuk melakukan validasi apakah bearer token yang diberikan pada saat memanggil API valid atau tidak. 
 
 router.get("/:id", verifyAccessToken, BarangController.getBarangById);
 
